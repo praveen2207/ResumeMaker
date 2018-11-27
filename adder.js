c4285671cf9c5e2e7bf00be9a1b8870d45027caa
PRAVEEN GANDHI VIDYAVASTU <U6IF@eurofins.com>
2018-11-27 16:07:09 +0530

    add and remove buttons improvised

--- adder.js
+++ adder.js
@@ -4,43 +4,45 @@ function increment(){
     i+=1;
 }
 
+function decrement(){
+    i-=1;
+}
+
 function textareaFunction(){
-    var r = document.createElement('span');
+    var r = document.createElement("span");
     var x = document.createElement("input");
-    var y = document.createElement("TEXTAREA");
-    var g = document.createElement("input");
+    var y = document.createElement("textarea");
     var linebreak = document.createElement("br");
+    var linebreak1 = document.createElement("br");
     var linebreak2 = document.createElement("br");
     y.setAttribute("cols",100);
     y.setAttribute("rows",4);
     y.setAttribute("placeholder","Work Experience");
-    y.setAttribute("style","position:relative");
     x.setAttribute("placeholder","Job Title");
     x.setAttribute("type","text");
     x.setAttribute("name","jobTitle");
+    x.setAttribute("class","input-group col-6");
+    y.setAttribute("class","input-group col-6");
     increment();
-    r.appendChild(y);
+    linebreak.setAttribute("id","lb0_"+i);
+    linebreak1.setAttribute("id","lb1_"+i);
+    linebreak2.setAttribute("id","lb2_"+i);
     x.setAttribute("id","jobTitle_"+i);
-    g.setAttribute("type","button");
-    g.setAttribute("onclick", "removeElement('myForm','id_"+i+"')");
-    g.setAttribute("class","btn btn-primary ml-2");
-    g.setAttribute("value","Remove");
-    r.appendChild(g);
     r.setAttribute("id","workExperience_"+i);
-    document.getElementById("myForm").appendChild(x);
-    document.getElementById("myForm").appendChild(linebreak);
-    document.getElementById("myForm").appendChild(linebreak2);
-    document.getElementById("myForm").appendChild(r);
+    r.appendChild(linebreak);
+    r.appendChild(x);
+    r.appendChild(linebreak1);
+    r.appendChild(y);
+    r.appendChild(linebreak2);
+    document.getElementById("workSection").appendChild(r);
 }
-function removeElement(parentDiv, childDiv){
-    if(childDiv == parentDiv)
+function removeElement(){
+    if(i == 0)
         alert("Element cannot be removed!");
-    else if(document.getElementById(childDiv)){
-        var child = document.getElementById(childDiv);
-        var parent = document.getElementById(parentDiv);
+    else {
+        var child = document.getElementById("workExperience_"+i);
+        var parent = document.getElementById("workSection");
         parent.removeChild(child);
-    }
-    else{
-        alert("Element has already been removed or doesn't exist!");
+        decrement();
     }
 }
